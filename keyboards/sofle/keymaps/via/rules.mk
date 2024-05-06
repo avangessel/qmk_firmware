111bac665371104d05ca862fab69f2e16fa11554
avangessel <andyv@ngessel.com>
2024-05-06 16:28:41 -0400

    Updated RGB info and compile rules

--- keyboards/sofle/keymaps/via/rules.mk
+++ keyboards/sofle/keymaps/via/rules.mk
@@ -1,8 +1,13 @@
 VIA_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 #RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+LTO_ENABLE = yes
+RAW_ENABLE = yes            # Enable RAW HID communication to PC
 
 # Reduce compiled size
 MOUSEKEY_ENABLE = no
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
+Link_Time_Optimization = yes # Reduce size of firmware by optimizing at link time
+EXTRAFLAGS += -flto
